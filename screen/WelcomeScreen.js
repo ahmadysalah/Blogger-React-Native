40b39dd16b80d24bbbf16ce66a7c548bc4ab8248
ahmadysalah <a.ahmad.salah@gmail.com>
2020-07-15 11:35:17 +0300

    Welcome screen First page #2

--- screen/WelcomeScreen.js
+++ screen/WelcomeScreen.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet, Image, View, Text,
+} from 'react-native';
+import welcomePg from '../assets/bg.png';
+import logo from '../assets/Logo/logo.png';
 
-export default function WelcomeScreen() {
+export default function WelcomeScreen({ history: { push } }) {
+  setTimeout(() => {
+    push('/home');
+  }, 2 * 1000);
   return (
-        <View>
-            <Text></Text>
-        </View>
+      <View style={styles.container}>
+      <Text style={styles.bioText}>Designed By: Ahmad Salah</Text>
+      <Image source={welcomePg} style={styles.background}/>
+      <Image style={styles.logo} source={logo} />
+      </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    opacity: 0.4,
+  },
+  logo: {
+    position: 'absolute',
+    opacity: 1,
+  },
+  bioText: {
+    position: 'absolute',
+    bottom: 50,
+  },
+});
